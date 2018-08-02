19ebe5784d633d410a9c73d1e521285ca7aacb83
Antonio Esteban <aecdigitaltransformer@gmail.com>
2018-08-02 15:13:45 +0200

    dia3-commit2

--- starter-code/server/routes/api/employees.controller.js
+++ starter-code/server/routes/api/employees.controller.js
@@ -1,27 +1,29 @@
-const express  = require('express');
-const router   = express.Router();
-const loggedIn = require('../../utils/isAuthenticated');
-const Employee = require('../../models/employee');
-const EmployeeExperience = require('../../models/employee_experience');
+const express = require("express");
+const router = express.Router();
+const loggedIn = require("../../utils/isAuthenticated");
+const Employee = require("../../models/employee");
+const EmployeeExperience = require("../../models/employee_experience");
+const ActionPlan = require("../../models/actionPlans");
 
-router.get('/', (req, res, next) => {
-  Employee
-    .find({})
-    .exec( (err, employees) => {
-      if (err) { return res.status(500).json(err); }
-
-      return res.status(200).json(employees);
-    });
+router.get("/", (req, res, next) => {
+  Employee.find({}).exec((err, employees) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).json(employees);
+  });
 });
 
-router.get('/:id', (req, res, next) => {
-  Employee.findById(req.params.id).then(employee=>{
-      EmployeeExperience.findOne({'Employee': employee._id}).then(exp=>{
-        console.log(exp)
-        return res.status(200).json({employee, exp});
-      })
+router.get("/:id", (req, res, next) => {
+  Employee.findById(req.params.id).then(employee => {
+      EmployeeExperience.findOne({ Employee: employee._id }).then(exp => {
+        ActionPlan.findOne({ Employee: employee._id }).then(train => {
+          console.log(train);
+          return res.status(200).json({ employee, exp, train });
+        });
+      });
     })
-    .catch(err => res.status(500).json(err))
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
