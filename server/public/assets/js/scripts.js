b5c96ded58031ffa88256bc9d50ec7f2f94dafb8
Antonio Esteban <aecdigitaltransformer@gmail.com>
2018-08-08 17:23:42 +0200

    dia8-commit5

---
+++ server/public/assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+  $("#menu-toggle").click(function(e) {
+     e.preventDefault();
+    $("#wrapper").toggleClass("toggled"); 
+});
+})
